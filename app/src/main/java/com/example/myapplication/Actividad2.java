70b6fb40f01386ceb9afcbd23244af04c0c2065e
Reicas <rei-cas@outlook.com>
2025-09-01 18:58:12 -0400

    proyecto abott base 2 activities y intent

---
+++ app/src/main/java/com/example/myapplication/Actividad2.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
+
+
+public class Actividad2 extends AppCompatActivity {
+    private TextView Resultado;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Resultado = findViewById(R.id.Resultado);
+
+        // Se crea el string para almacenar el msj
+        String dato = getIntent().getStringExtra(MainActivity.msj);
+
+        // Lo muestra llega el string
+        Resultado.setText(dato);
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+public class MainActivity extends AppCompatActivity {
+    private EditText etMensaje;
+    private Button btnEnviar;
+    public static final String msj = "com.example.intentsapp.msj";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etMensaje = findViewById(R.id.etMensaje);
+        btnEnviar = findViewById(R.id.btnEnviar);
+
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                enviarDatos();
+            }
+        });
+    }
+
+    private void enviarDatos() {
+        String mensaje = etMensaje.getText().toString();
+
+            Intent intent = new Intent(MainActivity.this, Actividad2.class);
+            intent.putExtra(msj, mensaje);
+            startActivity(intent);
+        }
+    }
