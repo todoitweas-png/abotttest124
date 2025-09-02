df2c516ec4c5cc3429af23ae0ce4bbe54311c137
Reicas <rei-cas@outlook.com>
2025-09-02 16:30:35 -0400

    proyecto abott base 2 activities y intent RBV

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,18 +2,11 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     private EditText etMensaje;
     private Button btnEnviar;
